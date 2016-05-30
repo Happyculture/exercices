60c5e8354b61931e4b8cc01e426e4118ec5c9c5c
Julien Dubois <julien@happyculture.coop>
2016-05-30 12:21:45 +0200

    Move the Access class where it belongs.

--- happyculture/happy_alexandrie/src/Access/AlexandrieAccess.php
+++ happyculture/happy_alexandrie/src/Access/AlexandrieAccess.php
@@ -1,17 +1,14 @@
 <?php
 
-namespace Drupal\happy_alexandrie\Controller;
+namespace Drupal\happy_alexandrie\Access;
 
-use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Access\AccessResult;
 
 class AlexandrieAccess {
   /**
    * Limit access to the Library between 9:00 and 18:30.
-   *
-   * @param \Drupal\Core\Session\AccountInterface $account
    */
-  public function accessLibrary(AccountInterface $account) {
+  public function accessLibrary() {
     if (time() >= strtotime('today 9:00') && time() <= strtotime('today 18:30')) {
       return AccessResult::allowed();
     }
