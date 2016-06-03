e9360c3210ed7351540c527c7b98458223773332
Julien Dubois <julien@happyculture.coop>
2016-06-03 16:44:51 +0200

    Restore the Account argument.

--- happyculture/happy_alexandrie/src/Access/AlexandrieAccess.php
+++ happyculture/happy_alexandrie/src/Access/AlexandrieAccess.php
@@ -3,12 +3,13 @@
 namespace Drupal\happy_alexandrie\Access;
 
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Session\AccountInterface;
 
 class AlexandrieAccess {
   /**
    * Limit access to the Library between 9:00 and 18:30.
    */
-  public function accessLibrary() {
+  public function accessLibrary(AccountInterface $account) {
     if (time() >= strtotime('today 9:00') && time() <= strtotime('today 18:30')) {
       return AccessResult::allowed();
     }
