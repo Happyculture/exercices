5e34847817da03d8fcecd1c6db59bda89e98d77c
Julien Dubois <julien@happyculture.coop>
2016-07-22 16:44:42 +0200

    Comment the extra methods implemented in the field widget.

--- isbn/src/Plugin/Field/FieldWidget/IsbnWidget.php
+++ isbn/src/Plugin/Field/FieldWidget/IsbnWidget.php
@@ -27,7 +27,8 @@ use Symfony\Component\Validator\ConstraintViolationListInterface;
 class IsbnWidget extends WidgetBase {
 
   /**
-   * {@inheritdoc}
+   * Default values fetched by the module if the user never configure the
+   * widget settings.
    */
   public static function defaultSettings() {
     return array(
@@ -37,7 +38,9 @@ class IsbnWidget extends WidgetBase {
   }
 
   /**
-   * {@inheritdoc}
+   * Form exposed to collect specific data later used by the widget.
+   *
+   * In this example, we collect a placeholder text injected in the text fields.
    */
   public function settingsForm(array $form, FormStateInterface $form_state) {
     $elements = parent::settingsForm($form, $form_state);
@@ -59,7 +62,11 @@ class IsbnWidget extends WidgetBase {
   }
 
   /**
-   * {@inheritdoc}
+   * Form displayed when the field is edited.
+   *
+   * If the field cardinality is greater than one, this form is repeated as
+   * many times as required.
+   * You can see in this form, how to access to a widget setting.
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
     $default_isbn_value = NULL;
@@ -85,11 +92,14 @@ class IsbnWidget extends WidgetBase {
   }
 
   /**
-   * {@inheritdoc}
+   * You may want to implement this method if your widget has a multifield
+   * structure. (If you expose more than just a textfield for instance.
+   *
+   * This method will indicate which part of the form element to highlight
+   * to indicate that the form validation doesn't pass.
+   * It can return FALSE to bypass the validation.
    */
   public function errorElement(array $element, ConstraintViolationInterface $error, array $form, FormStateInterface $form_state) {
     return $element[$error->arrayPropertyPath[0]];
   }
-
-
 }
