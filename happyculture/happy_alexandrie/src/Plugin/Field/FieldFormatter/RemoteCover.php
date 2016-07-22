2ab18f2315928eda5518de4ea3c75cbb915700c9
Julien Dubois <julien@happyculture.coop>
2016-07-22 16:08:07 +0200

    Rename the formatter class name to be coherent.

--- happyculture/happy_alexandrie/src/Plugin/Field/FieldFormatter/RemoteCover.php
+++ happyculture/happy_alexandrie/src/Plugin/Field/FieldFormatter/RemoteCover.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @file
- * Contains \Drupal\happy_alexandrie\Plugin\Field\FieldFormatter\RemoteCover.
+ * Contains \Drupal\happy_alexandrie\Plugin\Field\FieldFormatter\RemoteCoverFormatter.
  */
 
 namespace Drupal\happy_alexandrie\Plugin\Field\FieldFormatter;
@@ -20,7 +20,7 @@ use Drupal\Core\Field\FormatterBase;
  *   }
  * )
  */
-class RemoteCover extends FormatterBase {
+class RemoteCoverFormatter extends FormatterBase {
 
   /**
    * Value of the ISBN without '-' char.
