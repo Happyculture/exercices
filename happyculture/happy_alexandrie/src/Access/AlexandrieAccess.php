d602f04a7df872256bcf64afa7ed2b6946e2f520
Artusamak <julien@happyculture.coop>
2016-10-05 15:06:42 +0200

    Be explicit about the cache.

--- happyculture/happy_alexandrie/src/Access/AlexandrieAccess.php
+++ happyculture/happy_alexandrie/src/Access/AlexandrieAccess.php
@@ -8,6 +8,8 @@ use Drupal\Core\Session\AccountInterface;
 class AlexandrieAccess {
   /**
    * Limit access to the Library between 9:00 and 18:30.
+   * Caution: Due to the cache system, this example doesn't really work. The
+   * exercise is about its philosophy. :)
    */
   public function accessLibrary(AccountInterface $account) {
     if (time() >= strtotime('today 9:00') && time() <= strtotime('today 18:30')) {
