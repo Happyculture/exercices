fd5afb0ba45dece9e64f859c5ebbf80d139c4516
Artusamak <julien@happyculture.coop>
2016-10-13 15:33:15 +0200

    Move advanced solution for container injection in a dedicated formatter class.

--- happyculture/happy_alexandrie/src/Plugin/Field/FieldFormatter/RemoteCoverFormatter.php
+++ happyculture/happy_alexandrie/src/Plugin/Field/FieldFormatter/RemoteCoverFormatter.php
@@ -9,7 +9,6 @@ namespace Drupal\happy_alexandrie\Plugin\Field\FieldFormatter;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Field\FieldDefinitionInterface;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\happy_alexandrie\Service\GetCoverServiceInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -22,57 +21,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   }
  * )
  */
-class RemoteCoverFormatter extends FormatterBase implements ContainerFactoryPluginInterface {
-
-  /**
-   * The get cover service.
-   *
-   * @var \Drupal\happy_alexandrie\Service\GetCoverServiceInterface
-   */
-  protected $remote_cover_service;
-
-  /**
-   * Constructs a RemoteCover object.
-   *
-   * @param string $plugin_id
-   *   The plugin_id for the formatter.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
-   *   The definition of the field to which the formatter is associated.
-   * @param array $settings
-   *   The formatter settings.
-   * @param string $label
-   *   The formatter label display setting.
-   * @param string $view_mode
-   *   The view mode.
-   * @param array $third_party_settings
-   *   Any third party settings.
-   * @param \Drupal\happy_alexandrie\Service\GetCoverServiceInterface $remote_cover_service
-   *   The remove cover service plugin manager.
-   */
-  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, GetCoverServiceInterface $remote_cover_service) {
-    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
-
-    $this->remote_cover_service = $remote_cover_service;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $plugin_id,
-      $plugin_definition,
-      $configuration['field_definition'],
-      $configuration['settings'],
-      $configuration['label'],
-      $configuration['view_mode'],
-      $configuration['third_party_settings'],
-      $container->get('happy_alexandrie.get_cover_service')
-    );
-  }
-
+class RemoteCoverFormatter extends FormatterBase {
   /**
    * Value of the ISBN without '-' char.
    *
@@ -102,7 +51,7 @@ class RemoteCoverFormatter extends FormatterBase implements ContainerFactoryPlug
         // Part calling a theme function.
         $build[$delta] = array(
           '#theme' => 'happy_cover',
-          '#cover_url' => $this->remote_cover_service->getCover($item->undashed_value),
+          '#cover_url' => \Drupal::service('happy_alexandrie.get_cover_service')->getCover($item->undashed_value),
           '#cover_title' => $items->getEntity()->label(),
         );
       }
