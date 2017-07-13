115a3943fabfe40201bd3351fda029a14d650c2a
Edouard Cunibil <edouard@happyculture.coop>
2017-07-13 11:55:50 +0200

    2.2.2: Add a parameter to the /welcome route.

--- happy_alexandrie/src/Controller/AlexandrieController.php
+++ happy_alexandrie/src/Controller/AlexandrieController.php
@@ -9,12 +9,15 @@ class AlexandrieController extends ControllerBase {
   /**
    * /welcome route callback.
    *
+   * @param string $name
+   *   The name of the visitor to show.
+   *
    * @return array
    *   The render array of the page.
    */
-  public function helloWorld() {
+  public function helloWorld($name) {
     return [
-      '#markup' => $this->t('Welcome into the Great Library'),
+      '#markup' => $this->t('Welcome @name into the Great Library', ['@name' => $name]),
     ];
   }
 
