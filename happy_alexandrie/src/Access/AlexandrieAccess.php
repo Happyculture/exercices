adaf7a421c2c866c1a8c3804328dcfd12094b5f7
Edouard Cunibil <edouard@happyculture.coop>
2017-07-13 12:19:07 +0200

    2.3.2: Add a permission to bypass schedule-based access restriction.

--- happy_alexandrie/src/Access/AlexandrieAccess.php
+++ happy_alexandrie/src/Access/AlexandrieAccess.php
@@ -3,21 +3,25 @@
 namespace Drupal\happy_alexandrie\Access;
 
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Session\AccountInterface;
 
 class AlexandrieAccess {
 
   /**
-   * Limit access to the Library between 9:00 and 18:30.
+   * Limit access to the Library between 9:00 and 18:30 except for admins.
    *
-   * @return \Drupal\Core\Access\AccessResultInterface
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The current user account.
+   *
+   * @return \Drupal\Core\Access\AccessResultInterface The access result.
    *   The access result.
    */
-  public function accessLibrary() {
+  public function accessLibrary(AccountInterface $account) {
     $time = time();
     if ($time >= strtotime('today 9:00') && $time <= strtotime('today 18:30')) {
       return AccessResult::allowed();
     }
-    return AccessResult::forbidden();
+    return AccessResult::allowedIfHasPermission($account, 'administer alexandrie library');
   }
 
 }
