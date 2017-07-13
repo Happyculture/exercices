b8e05b10e3acc76814abe9d387d0cc058c55b01c
Edouard Cunibil <edouard@happyculture.coop>
2017-07-13 16:48:07 +0200

    2.4.3: Use schedule settings in the access method.

--- happy_alexandrie/src/Access/AlexandrieAccess.php
+++ happy_alexandrie/src/Access/AlexandrieAccess.php
@@ -3,9 +3,13 @@
 namespace Drupal\happy_alexandrie\Access;
 
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Datetime\DrupalDateTime;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\happy_alexandrie\Form\AlexandrieConfigForm;
 
 class AlexandrieAccess {
+
+  const DATE_FORMAT = 'G:i';
 
   /**
    * Limit access to the Library between 9:00 and 18:30 except for admins.
@@ -17,8 +21,12 @@ class AlexandrieAccess {
    *   The access result.
    */
   public function accessLibrary(AccountInterface $account) {
+    $config = \Drupal::config('happy_alexandrie.library_config');
+    $hour_opening = DrupalDateTime::createFromFormat(AlexandrieConfigForm::DATE_FORMAT, $config->get('opening'))->format(self::DATE_FORMAT);
+    $hour_closing = DrupalDateTime::createFromFormat(AlexandrieConfigForm::DATE_FORMAT, $config->get('closing'))->format(self::DATE_FORMAT);
+
     $time = time();
-    if ($time >= strtotime('today 9:00') && $time <= strtotime('today 18:30')) {
+    if ($time >= strtotime('today ' . $hour_opening) && $time <= strtotime('today ' . $hour_closing)) {
       return AccessResult::allowed();
     }
     return AccessResult::allowedIfHasPermission($account, 'administer alexandrie library');
