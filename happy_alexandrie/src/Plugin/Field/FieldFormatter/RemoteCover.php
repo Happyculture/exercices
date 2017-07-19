2ac44a607e490c935e8b53d6a1e15673f7629ce4
Edouard Cunibil <edouard@happyculture.coop>
2017-07-19 17:05:52 +0200

    2.8.1: Add size settings to the RemoteCover formatter.

--- happy_alexandrie/src/Plugin/Field/FieldFormatter/RemoteCover.php
+++ happy_alexandrie/src/Plugin/Field/FieldFormatter/RemoteCover.php
@@ -3,6 +3,7 @@
 namespace Drupal\happy_alexandrie\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\isbn\Plugin\Field\FieldFormatter\IsbnFormatter;
 
 /**
@@ -21,6 +22,53 @@ class RemoteCover extends IsbnFormatter {
   /**
    * {@inheritdoc}
    */
+  public static function defaultSettings() {
+    return [
+      'cover_size' => 'L',
+    ] + parent::defaultSettings();
+  }
+
+  protected function getSizes() {
+    return [
+      'S' => $this->t('Small'),
+      'M' => $this->t('Medium'),
+      'L' => $this->t('Large'),
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $elements = parent::settingsForm($form, $form_state);
+
+    $elements['cover_size'] = [
+      '#type' => 'select',
+      '#title' => t('Cover size'),
+      '#description' => t('Open library source file size.'),
+      '#default_value' => $this->getSetting('cover_size'),
+      '#options' => $this->getSizes(),
+    ];
+
+    return $elements;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+
+    $sizes = $this->getSizes();
+    $size = $this->getSetting('cover_size');
+    $summary[] = t('Size: @size', ['@size' => $sizes[$size]]);
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = [];
 
@@ -29,6 +77,7 @@ class RemoteCover extends IsbnFormatter {
       $elements[$delta] = [
         '#theme' => 'remote_cover',
         '#isbn' => $item->{$prop_name},
+        '#size' => $this->getSetting('cover_size'),
         '#title' => $items->getEntity()->label(),
       ];
     }
