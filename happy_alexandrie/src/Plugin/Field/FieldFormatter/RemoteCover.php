d32bfd08f310255c9df285ccd1367452529c1d9e
Edouard Cunibil <edouard@happyculture.coop>
2017-07-19 17:35:56 +0200

    2.10.0: Use dependency injection to inject cover service in the RemoteCover formatter.

--- happy_alexandrie/src/Plugin/Field/FieldFormatter/RemoteCover.php
+++ happy_alexandrie/src/Plugin/Field/FieldFormatter/RemoteCover.php
@@ -4,8 +4,10 @@ namespace Drupal\happy_alexandrie\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\happy_alexandrie\Service\GetCoverServiceInterface;
 use Drupal\isbn\Plugin\Field\FieldFormatter\IsbnFormatter;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin implementation of the 'remote_cover' formatter.
@@ -18,7 +20,54 @@ use Drupal\isbn\Plugin\Field\FieldFormatter\IsbnFormatter;
  *   }
  * )
  */
-class RemoteCover extends IsbnFormatter {
+class RemoteCover extends IsbnFormatter implements ContainerFactoryPluginInterface {
+
+  /**
+   * @var \Drupal\happy_alexandrie\Service\GetCoverServiceInterface
+   */
+  protected $coverService;
+
+  /**
+   * Constructs a RemoteCover object.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The formatter settings.
+   * @param string $label
+   *   The formatter label display setting.
+   * @param string $view_mode
+   *   The view mode.
+   * @param array $third_party_settings
+   *   Any third party settings.
+   * @param \Drupal\happy_alexandrie\Service\GetCoverServiceInterface $cover_service
+   *   The cover service.
+   */
+  public function __construct($plugin_id, $plugin_definition, \Drupal\Core\Field\FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, GetCoverServiceInterface $cover_service) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
+
+    $this->coverService = $cover_service;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('happy_alexandrie.get_cover')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -73,14 +122,11 @@ class RemoteCover extends IsbnFormatter {
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = [];
 
-    /** @var GetCoverServiceInterface $cover_service */
-    $cover_service = \Drupal::service('happy_alexandrie.get_cover');
-
     foreach ($items as $delta => $item) {
       $prop_name = $item->mainPropertyName();
       $elements[$delta] = [
         '#theme' => 'remote_cover',
-        '#url' => $cover_service->getCover($item->{$prop_name}, $this->getSetting('cover_size')),
+        '#url' => $this->coverService->getCover($item->{$prop_name}, $this->getSetting('cover_size')),
         '#title' => $items->getEntity()->label(),
       ];
     }
