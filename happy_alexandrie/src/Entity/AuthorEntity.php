2605aae732725cfc4d469ae4204407ae42d2f8d7
Artusamak <julien@happyculture.coop>
2017-11-02 18:29:12 +0100

    Change the custom field type created.

---
+++ happy_alexandrie/src/Entity/AuthorEntity.php
@@ -0,0 +1,215 @@
+<?php
+
+namespace Drupal\happy_alexandrie\Entity;
+
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityChangedTrait;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\user\UserInterface;
+
+/**
+ * Defines the Author entity.
+ *
+ * @ingroup happy_author
+ *
+ * @ContentEntityType(
+ *   id = "author_entity",
+ *   label = @Translation("Author"),
+ *   handlers = {
+ *     "view_builder" = "Drupal\Core\Entity\EntityViewBuilder",
+ *     "list_builder" = "Drupal\happy_alexandrie\AuthorEntityListBuilder",
+ *     "views_data" = "Drupal\happy_alexandrie\Entity\AuthorEntityViewsData",
+ *     "translation" = "Drupal\happy_alexandrie\AuthorEntityTranslationHandler",
+ *
+ *     "form" = {
+ *       "default" = "Drupal\happy_alexandrie\Form\AuthorEntityForm",
+ *       "add" = "Drupal\happy_alexandrie\Form\AuthorEntityForm",
+ *       "edit" = "Drupal\happy_alexandrie\Form\AuthorEntityForm",
+ *       "delete" = "Drupal\happy_alexandrie\Form\AuthorEntityDeleteForm",
+ *     },
+ *     "access" = "Drupal\happy_alexandrie\AuthorEntityAccessControlHandler",
+ *     "route_provider" = {
+ *       "html" = "Drupal\happy_alexandrie\AuthorEntityHtmlRouteProvider",
+ *     },
+ *   },
+ *   base_table = "author_entity",
+ *   data_table = "author_entity_field_data",
+ *   translatable = TRUE,
+ *   admin_permission = "administer author entities",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "name",
+ *     "uuid" = "uuid",
+ *     "uid" = "user_id",
+ *     "langcode" = "langcode",
+ *     "status" = "status",
+ *   },
+ *   links = {
+ *     "canonical" = "/admin/authors/author_entity/{author_entity}",
+ *     "add-form" = "/admin/authors/author_entity/add",
+ *     "edit-form" = "/admin/authors/author_entity/{author_entity}/edit",
+ *     "delete-form" = "/admin/authors/author_entity/{author_entity}/delete",
+ *     "collection" = "/admin/authors/author_entity",
+ *   },
+ *   field_ui_base_route = "author_entity.settings"
+ * )
+ */
+class AuthorEntity extends ContentEntityBase implements AuthorEntityInterface {
+
+  use EntityChangedTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function preCreate(EntityStorageInterface $storage_controller, array &$values) {
+    parent::preCreate($storage_controller, $values);
+    $values += [
+      'user_id' => \Drupal::currentUser()->id(),
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getName() {
+    return $this->get('name')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setName($name) {
+    $this->set('name', $name);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCreatedTime() {
+    return $this->get('created')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setCreatedTime($timestamp) {
+    $this->set('created', $timestamp);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOwner() {
+    return $this->get('user_id')->entity;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOwnerId() {
+    return $this->get('user_id')->target_id;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setOwnerId($uid) {
+    $this->set('user_id', $uid);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setOwner(UserInterface $account) {
+    $this->set('user_id', $account->id());
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isPublished() {
+    return (bool) $this->getEntityKey('status');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setPublished($published) {
+    $this->set('status', $published ? TRUE : FALSE);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    $fields['user_id'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Authored by'))
+      ->setDescription(t('The user ID of author of the Author entity.'))
+      ->setRevisionable(TRUE)
+      ->setSetting('target_type', 'user')
+      ->setSetting('handler', 'default')
+      ->setTranslatable(TRUE)
+      ->setDisplayOptions('view', [
+        'label' => 'hidden',
+        'type' => 'author',
+        'weight' => 0,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'entity_reference_autocomplete',
+        'weight' => 5,
+        'settings' => [
+          'match_operator' => 'CONTAINS',
+          'size' => '60',
+          'autocomplete_type' => 'tags',
+          'placeholder' => '',
+        ],
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['name'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Name'))
+      ->setDescription(t('The name of the Author entity.'))
+      ->setSettings([
+        'max_length' => 50,
+        'text_processing' => 0,
+      ])
+      ->setDefaultValue('')
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'string',
+        'weight' => -4,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'string_textfield',
+        'weight' => -4,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['status'] = BaseFieldDefinition::create('boolean')
+      ->setLabel(t('Publishing status'))
+      ->setDescription(t('A boolean indicating whether the Author is published.'))
+      ->setDefaultValue(TRUE);
+
+    $fields['created'] = BaseFieldDefinition::create('created')
+      ->setLabel(t('Created'))
+      ->setDescription(t('The time that the entity was created.'));
+
+    $fields['changed'] = BaseFieldDefinition::create('changed')
+      ->setLabel(t('Changed'))
+      ->setDescription(t('The time that the entity was last edited.'));
+
+    return $fields;
+  }
+
+}
---
+++ happy_alexandrie/src/Form/AuthorEntityDeleteForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\happy_alexandrie\Form;
+
+use Drupal\Core\Entity\ContentEntityDeleteForm;
+
+/**
+ * Provides a form for deleting Author entities.
+ *
+ * @ingroup happy_author
+ */
+class AuthorEntityDeleteForm extends ContentEntityDeleteForm {
+
+
+}
