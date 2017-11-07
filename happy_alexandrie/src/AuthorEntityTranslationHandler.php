82ef2dce0fa8b8f05fdb376ec6041f604b37949c
Artusamak <julien@happyculture.coop>
2017-11-07 17:45:27 +0100

    Add the Author entity.

---
+++ happy_alexandrie/src/AuthorEntityTranslationHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\happy_alexandrie;
+
+use Drupal\content_translation\ContentTranslationHandler;
+
+/**
+ * Defines the translation handler for author_entity.
+ */
+class AuthorEntityTranslationHandler extends ContentTranslationHandler {
+
+  // Override here the needed methods from ContentTranslationHandler.
+
+}
