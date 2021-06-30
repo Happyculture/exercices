f4766ec9316e7ca4a46fea434d20c3fb3205a42a
Julien Dubois <julien@happyculture.coop>
2021-06-30 14:13:29 +0200

    Update formatter for new author role field.

--- happy_alexandrie/src/Plugin/Field/FieldFormatter/RemoteCover.php
+++ happy_alexandrie/src/Plugin/Field/FieldFormatter/RemoteCover.php
@@ -4,8 +4,7 @@ namespace Drupal\happy_alexandrie\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\happy_alexandrie\Service\GetCoverServiceInterface;
-use Drupal\isbn\Plugin\Field\FieldFormatter\IsbnFormatter;
+use Drupal\Core\Field\Plugin\Field\FieldFormatter\StringFormatter;
 
 /**
  * Plugin implementation of the 'remote_cover' formatter.
@@ -14,12 +13,11 @@ use Drupal\isbn\Plugin\Field\FieldFormatter\IsbnFormatter;
  *   id = "remote_cover",
  *   label = @Translation("Remote cover"),
  *   field_types = {
- *     "isbn"
+ *     "string"
  *   }
  * )
  */
-class RemoteCover extends IsbnFormatter {
-
+class RemoteCover extends StringFormatter {
   /**
    * {@inheritdoc}
    */
